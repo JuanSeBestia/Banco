e08f4819b010230ab8b9c0631072193556594edb
jsdussanc <cubidesjuan@gmail.com>
2014-04-29 13:36:22 -0500

    Update banco.java

--- banco.java
+++ banco.java
@@ -4,9 +4,12 @@
  */
 package banco;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import scala.Array;
 
 /**
  *
@@ -18,14 +21,20 @@ public class Banco {
      * @param args the command line arguments
      * Aqui estaran el # de operaciones (0,1) y las mismas operaciones (2,n)
      */
-    private static Integer cuenta = new Integer(0);
     private static String[] operands;
     public static Scanner sc = new Scanner(System.in);
+    static public ActorSystem system = ActorSystem.create("Transacciones");
+    static public ActorRef BancoServerCuenta = system.actorOf(Props.create(BancoServer.class), "Servidor");
+    //Solo nesesite un Actor para los dos Usuarios ya que su comportamiento es similar
+    static public ActorRef UserA = system.actorOf(Props.create(User.class), "UserA");
+    static public ActorRef UserB = system.actorOf(Props.create(User.class), "UserB");
 
     public static void main(String[] args) {
+        //Si desea ver si es por argumentos o por I/O 
+        //System.out.println("Argumentos" + args.length);
 
-        System.out.println("Argumentos" + args.length);
         if (args.length == 0) {
+            //Si no hay argumentos Escanearlos
             Integer n = sc.nextInt();
             Integer m = sc.nextInt();
             String[] aux = new String[n + m + 2];
@@ -34,40 +43,115 @@ public class Banco {
             for (int i = 2; i < n + m + 2; i++) {
                 aux[i] = sc.next();
             }
-            
+
             operands = aux;
         } else {
-
+            //Si hay argumentos cogerlos
             operands = args;
         }
-        try {
-            ThreadOperation hilo1 = new ThreadOperation(0, operands);
-            ThreadOperation hilo2 = new ThreadOperation(1, operands);
-            System.out.println("Empezemos con las Transacciones :");
-            hilo1.start();
-            hilo2.start();
-            hilo1.join();
-            hilo2.join();
-            System.out.println("Saldo en el banco: " + cuenta);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Banco.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("Error XP:" + ex);
-        }
 
+
+        //System.out.println("Empezemos con las Transacciones :");
+        int op1 = Integer.parseInt(operands[0]);
+        int op2 = Integer.parseInt(operands[1]);
+        String[] operandsA = new String[op1];
+        String[] operandsB = new String[op2];
+
+        Array.copy(operands, 2, operandsA, 0, op1);
+        Array.copy(operands, op1 + 2, operandsB, 0, op2);
+        /*Prueba ArrayCopy
+        for (String b : operandsB) {
+        System.out.println("losB"+b);
+        }
+        for (String a : operandsA) {
+        System.out.println("losA"+a);
+        }
+         * 
+         */
+        //Se le dice Atraves de mesnasjes a cada Cliente que debe hacer
+        UserA.tell(operandsA, null);
+        UserB.tell(operandsB, null);
+
+        system.shutdown();//Manda a terminacion de los actores
+        system.awaitTermination();//JOIN//Esperan a que terminen
+        Pruebas de Pedir el slado con mensaje pero hacen conflicto con la funcion system.shoutdown()
+        //BancoServerCuenta.tell("Imprimir", null);
+        //ActorRef BancoServerImprimir = system.actorOf(Props.create(BancoServer.class), "Imprimir");
+        // BancoServerImprimir.tell("Imprimir", null);
+        system.shutdown();//Manda a terminacion de los actores
+        system.awaitTermination();//JOIN//Esperan a que terminen
+        //Imprime el saldo por una funcion
+        System.out.println("Saldo en el banco: " + BancoServer.getCuenta());
+
+
+
+
+    }
+}
+
+class BancoServer extends UntypedActor {
+
+    private static int cuenta;
+    private static boolean otroYaTermino = false;
+
+    @Override
+    public void onReceive(Object operacion) throws Exception {
+        //System.out.println("Ahh llegado un mensaje" + operacion.toString());
+        if (operacion instanceof String) {
+
+            String preps = (String) operacion;
+            //Impirime el saldo solo cuando el primero termina
+            if (preps.equals("Imprimir")) {
+                if (otroYaTermino) {
+                    System.out.println("Mensaje->Saldo en el banco: " + BancoServer.getCuenta());
+                } else {
+                    otroYaTermino = true;
+                }
+            } //La suma en la cuenta
+            else {
+                int aux = Integer.parseInt((String) operacion);
+                cuenta = getCuenta() + aux;
+            }
+        } else {
+
+            unhandled(operacion);
+
+        }
 
     }
 
     /**
      * @return the cuenta
      */
-    public static Integer getCuenta() {
+    public static int getCuenta() {
         return cuenta;
     }
+}
 
-    /**
-     * @param aCuenta the cuenta to set
-     */
-    public static void setCuenta(Integer aCuenta) {
-        cuenta = aCuenta;
+class User extends UntypedActor {
+
+    private int numop = 0;
+    private String Operands[];
+
+    @Override
+    public void onReceive(Object operacion) throws Exception {
+        if (operacion instanceof String[]) {
+
+            Operands = (String[]) operacion;
+            numop = Operands.length;
+            for (int i = 0; i < numop; i++) {
+                System.out.println("Se esta sumando " + Operands[i] + " con " + aux );
+                //Sesion Critica
+                Banco.BancoServerCuenta.tell(Operands[i], null);
+
+            }
+            Banco.BancoServerCuenta.tell("Imprimir", null);
+
+        } else {
+
+            unhandled(operacion);
+
+        }
+
     }
 }
